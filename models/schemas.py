27d2e7ce073ceb53b593b5b4ac75cb3e35a67ee3
MrTheBank <thebankzana@yahoo.com>
2023-02-07 21:47:16 +0700

    country model

--- models/schemas.py
+++ models/schemas.py
@@ -6,6 +6,14 @@ client = mongox.Client(MONGODB_URI)
 db = client.get_database(MONGODB_COLLECTION)
 
 
+# https://aminalaee.dev/mongox/
 class TestSchema(mongox.Model, db=db):
     text1: str
     text2: int
+
+
+class Country(mongox.Model, db=db, collection="countries"):
+    name: str
+    alpha_2: str = mongox.Field(..., min_length=2, max_length=2)
+    alpha_3: str = mongox.Field(..., min_length=3, max_length=3)
+    region: str
